34eec19fc92c425f66354e11cdea84d875034d0e
nick <huggablesquare@gmail.com>
2023-01-29 04:23:26 +0000

    add iheart support

--- auxy/server.py
+++ auxy/server.py
@@ -1,5 +1,6 @@
 import streamlink
-from flask import Flask, redirect
+import requests
+from flask import redirect
 from auxy import app
 
 @app.route('/ping')
@@ -9,3 +10,8 @@ def ping():
 @app.route('/twitch/<user>')
 def twitch_user(user):
     return redirect(streamlink.streams(f'https://www.twitch.tv/{user}')['audio_only'].url)
+
+@app.route('/iheart/<station>')
+def iheart_station(station):
+    response = requests.get(f'https://us.api.iheart.com/api/v2/content/liveStations/{station}')
+    return redirect(response.json()['hits'][0]['streams']['secure_shoutcast_stream'])
