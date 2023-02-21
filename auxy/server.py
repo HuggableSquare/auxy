97279e0065403cb09b86c89193bae9542400c238
nick <huggablesquare@gmail.com>
2023-02-21 19:55:40 +0000

    fix one of the iheart redirects

--- auxy/server.py
+++ auxy/server.py
@@ -17,7 +17,7 @@ def iheart_station(station):
     station_response = requests.get(f'https://us.api.iheart.com/api/v2/content/liveStations/{station}')
     streams = station_response.json()['hits'][0]['streams']
     if 'secure_shoutcast_stream' in streams:
-        return redirect(response.json()['hits'][0]['streams']['secure_shoutcast_stream'])
+        return redirect(streams['secure_shoutcast_stream'])
     elif 'secure_pls_stream' in streams:
         pls_response = requests.get(streams['secure_pls_stream'])
         parser = ConfigParser()
